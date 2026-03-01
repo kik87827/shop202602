03d353c916197eb6a3aea6a03c4b00a2435b3962
kik87827 <82139647+kik87827@users.noreply.github.com>
2026-03-01 23:32:13 +0900

    테스트

--- src/Components/CardData.jsx
+++ src/Components/CardData.jsx
@@ -8,7 +8,7 @@ export default function CardData({ item }) {
     <Col md={4} style={{ textAlign: "center" }} key={item.id} onClick={() => navigate(`/detail/${item.id}`)}>
       <img src={`${process.env.PUBLIC_URL}/shoes${item.id + 1}.jpg`} style={{ width: "100%" }} alt="" />
       <h4>{item.title}</h4>
-      <p>{item.content}</p>
+      <p>{item.content} </p>
     </Col>
   );
 }
