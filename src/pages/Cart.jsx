f7a0018a4f57f56072f0e7d50f74c188fd665fbe
in kyung <kik87827@gmail.com>
2026-04-13 15:05:45 +0900

    memo 스터디

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { memo, useMemo, useState } from "react";
 import { Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { countChange, productDelete } from "./../store/userCart";
 import { changeName, changeAge } from "./../store/userSlice";
 
+let Child = memo(() => {
+  console.log("재렌더링됨");
+  return <div>Child</div>;
+});
+
+function FuncAction() {
+  return "반복문 10억번";
+}
+
 export default function Cart() {
   let { user, cartData } = useSelector((state) => state);
   let dispatch = useDispatch();
-
-  console.log(cartData);
+  let [count, setCount] = useState(0);
+  let result = useMemo(() => {
+    return FuncAction();
+  }, [count]);
 
   return (
     <div>
+      <Child count={count} />
+      <button
+        onClick={() => {
+          setCount((prev) => prev + 1);
+        }}
+      >
+        +
+      </button>
       {user.name} {user.age}의 장바구니
       <br />
       <button onClick={() => dispatch(changeName())}>이름변경</button>
